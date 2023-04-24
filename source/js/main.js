f91411590191174ee448a03273a15919f844d6b6
Irina <irinka-akva@ya.ru>
2023-04-25 00:02:32 +0300

    Добавляет верстку и адаптив раздела Абонементы

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {Form} from './modules/form-validate/form';
 import {videoPlayHandler} from './modules/video-player';
+import {initTabs} from './modules/tabs/init-tabs';
 
 // ---------------------------------
 
@@ -21,6 +22,7 @@ window.addEventListener('DOMContentLoaded', () => {
     window.form = form;
     form.init();
     videoPlayHandler();
+    initTabs();
   });
 });
 
--- source/js/modules/video-player.js
+++ source/js/modules/video-player.js
@@ -1,17 +1,18 @@
 const videoWrapper = document.querySelector('[data-video-wrapper]');
-const iframeVideo = videoWrapper.querySelector('[data-iframe-video]');
+const video = videoWrapper.querySelector('[data-video]');
 const videoCover = videoWrapper.querySelector('[data-cover]');
 const videoButton = videoWrapper.querySelector('[data-video-button]');
 
 const makeVideoActive = () => {
-  if (iframeVideo) {
-    videoCover.style.display = 'none';
-    iframeVideo.src += '?autoplay=1';
-  }
+  videoCover.style.display = 'none';
+  const newVideo = '<iframe src="https://www.youtube.com/embed/9TZXsZItgdw?autoplay=1" frameborder = "0" allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe > ';
+  video.insertAdjacentHTML('afterbegin', newVideo);
 };
 
 const videoPlayHandler = () => {
-  videoButton.addEventListener('click', makeVideoActive);
+  videoButton.addEventListener('click', makeVideoActive, {passive: true});
+  videoButton.addEventListener('touchstart', makeVideoActive, {passive: true});
 };
+
 
 export {videoPlayHandler};
